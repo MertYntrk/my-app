fc4763a43bd7a1570b1f40bd647d9005ff7c024b
MertYntrk <mert.yenturk@hotmail.com>
2022-06-20 11:08:09 +0100

    initial commit

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+// import { Product, products } from '../products';
+// import { CartService } from '../cart.service';
+
+// export class ProductDetailsComponent implements OnInit {
+
+//   constructor(
+//     private route: ActivatedRoute,
+//     private cartService: CartService
+//   ) { }
+
+//   addToCart(product: Product) {
+//     this.cartService.addToCart(product);
+//     window.alert('Your product has been added to the cart!');
+//   }
+// }
+
+export class ProductDetailsComponent {
+
+  constructor() { }
+}
